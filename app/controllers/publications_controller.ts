f1991f24518e230969fa102b451786b94b6ca7c7
Argyrou <argykaselve@gmail.com>
2025-05-30 12:02:28 +0200

    Ajout des models et migrations

--- app/controllers/publications_controller.ts
+++ app/controllers/publications_controller.ts
@@ -1,4 +1,15 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Media from '#models/media'
+import Publication from '#models/publication'
+
 
 export default class PublicationsController {
+    async create({request}:HttpContext){
+        let texteTweet = request.input('texteTweet')
+        let image = request.file('image')
+
+
+
+
+    }
 }
