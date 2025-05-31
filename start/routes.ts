c2927e0665983fff6cffe63a284a22cbba051b19
Argyrou <argykaselve@gmail.com>
2025-05-31 12:59:16 +0200

    Ajout du style dans la page login

--- start/routes.ts
+++ start/routes.ts
@@ -12,5 +12,6 @@ import router from '@adonisjs/core/services/router'
 
 
 router.on('/').render('pages/home')
+router.on('/login').render('security/login')
 router.on('/profil').render('pages/profil')
 router.post('/create' , [PublicationsController , 'create'])
