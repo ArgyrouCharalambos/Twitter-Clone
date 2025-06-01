5bb303df5fa508f658e2ad59388ad9d83fb4bf8a
Argyrou <argykaselve@gmail.com>
2025-06-01 11:01:37 +0200

    Ajout de l'authentification

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,4 +1,29 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user';
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
+    async create({request ,response}:HttpContext){
+        let data = request.only(["nom","prenom","email","password"]);
+
+        await User.create({
+            nom:data.nom,
+            prenom:data.prenom,
+            email:data.email,
+            password:data.password
+
+        })
+
+        response.redirect('/login')
+
+    }
+    async connexion({request ,auth , response}:HttpContext){
+        const data = request.all();
+
+        const user = await User.verifyCredentials(data.email, data.password)
+
+        await auth.use('web').login(user)
+    
+        response.redirect('/')
+        
+    }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -10,8 +10,13 @@
 import PublicationsController from '#controllers/publications_controller'
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import UsersController from '#controllers/users_controller'
 
 router.on('/login').render('security/login')
+
+router.post('/signin' , [UsersController , 'create'])
+router.get('/connexion' , [UsersController , 'connexion'])
+
 
 router.on('/').render('pages/home').use(middleware.auth()) 
 router.on('/profil').render('pages/profil').use(middleware.auth()) 
