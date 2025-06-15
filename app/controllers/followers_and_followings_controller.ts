1bbf2be56cc2e46463e5b74d4e2a9bc5daeb9627
Argyrou <argykaselve@gmail.com>
2025-06-15 14:41:45 +0200

    organisation du code sur tout les controllers

--- app/controllers/followers_and_followings_controller.ts
+++ app/controllers/followers_and_followings_controller.ts
@@ -1,60 +1,65 @@
 import Following from '#models/following'
-import User from '#models/user';
+import User from '#models/user'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class FollowersAndFollowingsController {
   async followers({ auth, params, response }: HttpContext) {
-
-    const userId:number = Number(auth.user?.id);
-    const paramsId = Number(params.id);
+    const userId: number = Number(auth.user?.id)
+    const paramsId = Number(params.id)
 
     const verification = await Following.query()
       .where('id_utilisateur', userId)
-      .andWhere('id_utilisateur_abonnement', paramsId).first();
+      .andWhere('id_utilisateur_abonnement', paramsId)
+      .first()
 
     if (verification) {
-      await verification.delete();
+      await verification.delete()
     } else {
       await Following.create({
-        idUtilisateur:userId,
+        idUtilisateur: userId,
         idUtilisateurAbonnement: paramsId,
-      });
+      })
     }
-   response.redirect().back()
-
+    response.redirect().back()
   }
-  async getFollowers({view ,auth}:HttpContext){
+  async getFollowers({ view, auth }: HttpContext) {
+    const userAll = await User.query().whereNot('id', Number(auth.user?.id))
 
-    const userAll = await User.query().whereNot('id',Number(auth.user?.id) );
+    const existeOuPas = await Following.findManyBy('id_utilisateur', auth.user?.id)
 
+    let tableauAbonnement = []
 
-    const existeOuPas = await Following.findManyBy("id_utilisateur" , auth.user?.id);
-    
-    let tableauAbonnement = [];
-
-    for(let e of existeOuPas){
-        tableauAbonnement.push(e.idUtilisateurAbonnement)
+    for (let e of existeOuPas) {
+      tableauAbonnement.push(e.idUtilisateurAbonnement)
     }
-    
-    const abonné = await Following.query().where("id_utilisateur_abonnement", Number(auth.user?.id)).whereNot('id_utilisateur',Number(auth.user?.id) ).preload("user")
 
-    return view.render("pages/followers" ,{abonné,tableauAbonnement,userAll,user:auth.user})
+    const abonné = await Following.query()
+      .where('id_utilisateur_abonnement', Number(auth.user?.id))
+      .whereNot('id_utilisateur', Number(auth.user?.id))
+      .preload('user')
+
+    return view.render('pages/followers', { abonné, tableauAbonnement, userAll, user: auth.user })
   }
-  async getFollowings({view,auth}:HttpContext){
+  async getFollowings({ view, auth }: HttpContext) {
+    const userAll = await User.query().whereNot('id', Number(auth.user?.id))
 
-            const userAll = await User.query().whereNot('id',Number(auth.user?.id) );
-    
-            const existeOuPas = await Following.findManyBy("id_utilisateur" , auth.user?.id);
-    
-            let tableauAbonnement = [];
-    
-            for(let e of existeOuPas){
-                tableauAbonnement.push(e.idUtilisateurAbonnement)
-            }
-    
+    const existeOuPas = await Following.findManyBy('id_utilisateur', auth.user?.id)
 
-    const abonnement = await Following.query().where("id_utilisateur",Number(auth.user?.id)).preload("userAbonnement")
+    let tableauAbonnement = []
 
-    return view.render("pages/followings",{abonnement,tableauAbonnement,userAll,user:auth.user})
+    for (let e of existeOuPas) {
+      tableauAbonnement.push(e.idUtilisateurAbonnement)
+    }
+
+    const abonnement = await Following.query()
+      .where('id_utilisateur', Number(auth.user?.id))
+      .preload('userAbonnement')
+
+    return view.render('pages/followings', {
+      abonnement,
+      tableauAbonnement,
+      userAll,
+      user: auth.user,
+    })
   }
 }
